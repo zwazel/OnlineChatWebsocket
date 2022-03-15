377bf983ae827d750e3f5526b357f868ebc0cfdc
Zwazel <zwazel@hotmail.com>
2022-03-15 16:47:29 +0100

    das da?

--- src/main/java/dev/zwazel/onlinechatwebsocket/OnlineChatWebSocketApplication.java
+++ src/main/java/dev/zwazel/onlinechatwebsocket/OnlineChatWebSocketApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineChatWebSocketApplication {
-    
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineChatWebSocketApplication.class, args);
     }
