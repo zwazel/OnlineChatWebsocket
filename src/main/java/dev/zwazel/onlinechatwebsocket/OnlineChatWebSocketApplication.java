45966dbe65c76474eac2a3e36dca10cd453a8df0
Zwazel <zwazel@hotmail.com>
2022-03-15 14:56:44 +0100

    INIT

---
+++ src/main/java/dev/zwazel/onlinechatwebsocket/OnlineChatWebSocketApplication.java
@@ -0,0 +1,13 @@
+package dev.zwazel.onlinechatwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChatWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineChatWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/zwazel/onlinechatwebsocket/OnlineChatWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zwazel.onlinechatwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineChatWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
