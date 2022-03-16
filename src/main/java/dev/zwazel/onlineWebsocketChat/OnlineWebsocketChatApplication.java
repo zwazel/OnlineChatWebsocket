1091f694d8676f4565e882543f76852b1506b7d2
Zwazel <zwazel@hotmail.com>
2022-03-16 14:52:15 +0100

    wird de da funktioniere?

---
+++ src/main/java/dev/zwazel/onlineWebsocketChat/OnlineWebsocketChatApplication.java
@@ -0,0 +1,13 @@
+package dev.zwazel.onlineWebsocketChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWebsocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineWebsocketChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zwazel/onlineWebsocketChat/OnlineWebsocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zwazel.onlineWebsocketChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineWebsocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
