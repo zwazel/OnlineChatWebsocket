462f85ace6a8911b7b8e8cb51df3d9a3e3cca526
Zwazel <zwazel@hotmail.com>
2022-03-17 15:08:12 +0100

    Revert "hm"
    This reverts commit 41498ea1ceba29261dadd7316599d1e7ffdd6cc7.

--- src/main/java/dev/zwazel/onlineWebsocketChat/OnlineWebsocketChatApplication.java
+++ src/main/java/dev/zwazel/onlineWebsocketChat/OnlineWebsocketChatApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineWebsocketChatApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineWebsocketChatApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineWebsocketChatApplication.class, args);
+	}
+
 }
